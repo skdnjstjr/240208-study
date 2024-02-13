6cb9f0a4f4c770a199d5b549c7cea25f65642b13
skdnjstjr <skdnjstjr0121@naver.com>
2024-02-13 18:10:37 +0900

    2024-02-13 수업내용+ test

---
+++ src/main/java/com/ohgiraffers/section02/looping/Application01.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+//        a.testSimpleForStatement();
+//        a.testForExample1();
+//        a.testForExample2();
+//        a.testForExample3();
+//        a.testForExample4();
+//        a.printSimpleGugudan();
+
+        A_nestedFor a2 = new A_nestedFor();
+//        a2.printGugudanFromTwoToNine();
+//        a2.printUpgradeGugudan();
+        a2.printStarInputRowTimes();
+    }
+
+
+
+
+}
